ca0f80d7d523e0e2ade143e90b946c53820b8e57
Andrea <alfanoandrea008@gmail.com>
2026-09-18 14:54:14 +0200

    Add app/lib/supabase.ts

---
+++ app/lib/supabase.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://rdxfhjjhvdztchwbqgea.supabase.co";
+const supabasePublishableKey = "sb_publishable_ITuNYxbjhH84r8s3kftaLA_o-txrLXZ";
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    flowType: "pkce",
+  },
+});
