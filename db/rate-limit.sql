c227a9212ff39c35aeae945838e68ab93af7ced8
Andrea <alfanoandrea008@gmail.com>
2026-09-20 09:55:39 +0200

    Index rate limit cleanup window

--- db/rate-limit.sql
+++ db/rate-limit.sql
@@ -3,3 +3,6 @@ CREATE TABLE IF NOT EXISTS rate_limits (
   window_start INTEGER NOT NULL,
   count INTEGER NOT NULL DEFAULT 0
 );
+
+CREATE INDEX IF NOT EXISTS idx_rate_limits_window_start
+ON rate_limits(window_start);
